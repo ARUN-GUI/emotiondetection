01113965ea7abaeda4d1f3c0602ea9c12eb9ebb4
ARUN S K <128890207+ARUN-GUI@users.noreply.github.com>
2023-09-06 02:50:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,69 +1,84 @@
 import streamlit as st
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
-#from utils import predict_label
-from PIL import Image
-import numpy as np
 import os
-st.title("emotion detection")
-
-st.write("Predict the emotion that is being represented in the image.")
-
-model = load_model("model.h5")
-l=[ 'angry',
-    'disgusted',
-    'fearful',
-    'happy',
-    'neutral',
-    'sad',
-    'surprised']
-
-#model = load_model("model.h5")
-model_path = os.path.join(os.getcwd(), "emotiondetection/model.h5")
-
-if os.path.exists(model_path):
-    model = load_model(model_path)
-else:
-    st.write("Model file 'model.h5' not found.")
-uploaded_file = st.file_uploader(
-    "Upload an image of a emotion :", type="jpeg"
-)
-predictions=-1
-if uploaded_file is not None:
-    image1 = Image.open(uploaded_file)
-    image1=image.smart_resize(image1,(112,112))
-    img_array = image.img_to_array(image1)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array/255.0
-    predictions = model.predict(img_array)
-    label=l[np.argmax(predictions)]
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import warnings
+warnings.filterwarnings('ignore')
+import pathlib
+from keras.models import load_model
+import matplotlib.image as mpimg
 
 
-st.write("### Prediction Result")
-if st.button("Predict"):
+# # Define the home function
+# def home():
+#     st.write("## Introduction")
+#     st.write("This app uses to dectect the emotion of the person")
+   
+    
+#     st.write("This Data contains around 75k images of size 50x50 distributed under 5 categories.")
+#     st.write("'Five' -> 0")
+#     st.write("'Zero' -> 1")
+#     st.write("'Two' -> 2")
+#     st.write("'Four' -> 3")
+#     st.write("'One' -> 4")
+#     st.write("'Three' -> 5")
+
+
+# Define the prediction function
+def prediction():
+    
+    st.write("Predict the Rice image that is being represented in the image")
+    
+    # Define the input fields
+    model = load_model("cnn.h5")
+
+
+    
+    uploaded_file = st.file_uploader(
+        "Upload an image of a Rice Image:", type="jpg"
+    )
+    predictions=-1
     if uploaded_file is not None:
         image1 = Image.open(uploaded_file)
-        st.image(image1, caption="Uploaded Image", use_column_width=True)
-        st.markdown(
-            f"<h2 style='text-align: center;'>Image of {label}</h2>",
-            unsafe_allow_html=True,
-        )
-    else:
-        st.write("Please upload file or choose sample image.")
+        image1=image.smart_resize(image1,(64,64))
+        img_array = image.img_to_array(image1)
+        img_array = np.expand_dims(img_array, axis=0)
+        img_array = img_array/255.0
+        predictions = model.predict(img_array)
+    st.write("### Prediction Result")
+    if st.button("Predict"): 
+        labels = {
+    'angry':0,
+    'disgusted':1,
+    'fearful':2,
+    'happy':3,
+    'neutral':4,
+    'sad':5,
+    'surprised':6
+}
+        if prediction!=-1:
+            if uploaded_file is not None:
+                image1 = Image.open(uploaded_file)
+                st.image(image1, caption="Uploaded Image", use_column_width=True)
+                st.markdown(
+                    f"<h2 style='text-align: center;'>Image of {labels[np.argmax(predictions)]}</h2>",
+                    unsafe_allow_html=True,
+                )
+            else:
+                st.write("Please upload file or choose sample image.")
 
-
-st.write("If you would not like to upload an image, you can use the sample image instead:")
-sample_img_choice = st.button("Use Sample Image")
-
-if sample_img_choice:
-    image1 = Image.open("seed_charlock.png")
-    image1=image.smart_resize(image1,(256,256))
-    img_array = image.img_to_array(image1)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array/255.0
-    predictions = model.predict(img_array)
-    label=l[np.argmax(predictions)]
-    st.markdown(
-        f"<h2 style='text-align: center;'>{label}</h2>",
-        unsafe_allow_html=True,
-    )
+def main():
+    st.set_page_config(page_title="Rice Image Classification", page_icon=":heart:")
+    st.markdown("<h1 style='text-align: center; color: white;'>Rice Image Classification</h1>", unsafe_allow_html=True)
+    
+    # Only show the "Classification" page
+    prediction()
+main()
