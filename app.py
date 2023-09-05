e15d8eeb63805de094a107a0bc245dc6fc6bad16
ARUN S K <128890207+ARUN-GUI@users.noreply.github.com>
2023-09-06 02:34:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ l=[ 'angry',
     'surprised']
 
 #model = load_model("model.h5")
-model_path = os.path.join(os.getcwd(), "model.h5")
+model_path = os.path.join(os.getcwd(), "emotiondetection
+/model.h5/model.h5")
 
 if os.path.exists(model_path):
     model = load_model(model_path)
